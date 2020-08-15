22d5ea3ec1a9f845dcd16607148c4da9996d1233
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-15 22:01:55 +0900

    in the middle of state

--- react-complete-guide/react-complete-guide/src/App.js
+++ react-complete-guide/react-complete-guide/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: "Takaki", age: "21" },
+      { name: "Bob", age: "21" },
+      { name: "John", age: "21" },
+      { name: "Yoko", age: "21" },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1> OMANKO </h1>
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-complete-guide/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const person = (props) => {
+  return (
+    <p>
+      I'm {props.name} and {props.age} yrs old! Dae
+    </p>
+  );
+};
+
+export default person;
